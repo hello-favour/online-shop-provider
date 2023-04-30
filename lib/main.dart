c42f115958d84b6ec314ced3bb8c4dc04760598e
Favour <hello.williamsfav@gmail.com>
2023-04-30 12:20:47 +0100

    set up provider state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:online_shop_app/controllers/mainscreen_provider.dart';
 import 'package:online_shop_app/main_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +12,22 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Online Shop',
-      theme: ThemeData(
-        fontFamily: "Inter",
+    final state = Provider.of<MainScreenNotifier>(context);
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) {
+            MainScreenNotifier();
+          },
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Online Shop',
+        theme: ThemeData(
+          fontFamily: "Inter",
+        ),
+        home: MainScreen(),
       ),
-      home: MainScreen(),
     );
   }
 }
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:online_shop_app/components/app_theme.dart';
 import 'package:online_shop_app/components/bottom_nav.dart';
+import 'package:online_shop_app/controllers/mainscreen_provider.dart';
 import 'package:online_shop_app/screens.dart/cart_page.dart';
 import 'package:online_shop_app/screens.dart/home_page.dart';
 import 'package:online_shop_app/screens.dart/profile_page.dart';
 import 'package:online_shop_app/screens.dart/search_page.dart';
+import 'package:provider/provider.dart';
 
 class MainScreen extends StatelessWidget {
   MainScreen({super.key});
@@ -18,48 +20,59 @@ class MainScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    int pageIndex = 1;
-    return Scaffold(
-      backgroundColor: AppTheme.bgrdColor,
-      body: pageList[pageIndex],
-      bottomNavigationBar: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(AppTheme.bottomNavPadding),
-          child: Container(
-            padding: const EdgeInsets.all(12),
-            margin: const EdgeInsets.symmetric(horizontal: 10),
-            decoration: const BoxDecoration(
-              color: Colors.black,
-              borderRadius: AppTheme.appRadius,
-            ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                BottomNavWidget(
-                  icon: Icons.home,
-                  onTap: () {},
-                ),
-                BottomNavWidget(
-                  icon: Icons.search,
-                  onTap: () {},
-                ),
-                BottomNavWidget(
-                  icon: Icons.add,
-                  onTap: () {},
-                ),
-                BottomNavWidget(
-                  icon: Icons.shopping_cart,
-                  onTap: () {},
-                ),
-                BottomNavWidget(
-                  icon: Icons.person,
-                  onTap: () {},
-                ),
-              ],
+    return Consumer<MainScreenNotifier>(builder: (context, state, child) {
+      return Scaffold(
+        backgroundColor: AppTheme.bgrdColor,
+        body: pageList[state.pageIndex],
+        bottomNavigationBar: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(AppTheme.bottomNavPadding),
+            child: Container(
+              padding: const EdgeInsets.all(12),
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              decoration: const BoxDecoration(
+                color: Colors.black,
+                borderRadius: AppTheme.appRadius,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  BottomNavWidget(
+                    icon: Icons.home,
+                    onTap: () {
+                      state.pageIndex = 0;
+                    },
+                  ),
+                  BottomNavWidget(
+                    icon: Icons.search,
+                    onTap: () {
+                      state.pageIndex = 1;
+                    },
+                  ),
+                  BottomNavWidget(
+                    icon: Icons.add,
+                    onTap: () {
+                      state.pageIndex = 2;
+                    },
+                  ),
+                  BottomNavWidget(
+                    icon: Icons.shopping_cart,
+                    onTap: () {
+                      state.pageIndex = 3;
+                    },
+                  ),
+                  BottomNavWidget(
+                    icon: Icons.person,
+                    onTap: () {
+                      state.pageIndex = 4;
+                    },
+                  ),
+                ],
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
